9eea75bbf0cd22f1f4baaa4f3a72efd93d863bf0
ahill7 <ahill7@uoregon.edu>
2017-01-19 17:27:12 -0800

    Fixed GUI to redraw address book after a new contact is added

--- GUI/addressBook.py
+++ GUI/addressBook.py
@@ -1,25 +1,28 @@
 from tkinter import *
-import tkinter as ttk
+import tkinter as tk 
+from tkinter import ttk
 
-class KabyAddrapp(ttk.Tk):
+contacts=[["First name", "Last name", "Address", "State", "Zip", "Email", "Phone Number"]];
+
+class KabyAddrapp(tk.Tk):
 
     def __init__(self, *args, **kwargs):
 
-        ttk.Tk.__init__(self, *args, **kwargs);
-        container = ttk.Frame(self); 
+        tk.Tk.__init__(self, *args, **kwargs);
+        #This command will add logo
+        #tk.Tk.conbitmap(self, default="");
+        self.container = tk.Frame(self); 
 
         #container.pack(side="top", fill="both", expand=True);
-        container.grid(row=0, column=0);
+        self.container.grid(row=0, ipadx=25, ipady=10);
 
-        container.grid_rowconfigure(0,weight=1);
-        container.grid_columnconfigure(0,weight=1);
-        #container.table=[];
-        container.contacts=["First name", "Last name", "Address", "State", "Zip", "Email", "Phone Number"];
+        self.container.grid_rowconfigure(0,weight=1);
+        self.container.grid_columnconfigure(0,weight=1);
         
         self.frames= {};
         
         for F in (StartPage, PageOne):
-            frame = F(container, self);
+            frame = F(self.container, self);
             self.frames[F] = frame;
             frame.grid(row=0, column=0, sticky="nsew");
 
@@ -28,6 +31,12 @@ class KabyAddrapp(ttk.Tk):
     def show_frame(self, cont):
         frame = self.frames[cont];
         frame.tkraise();
+
+
+    def refresh_frame(self, F):
+        frame = F(self.container, self);
+        self.frames[F] = frame;
+        frame.grid(row=0, column=0, sticky="nsew");
 
 
     def add_contact(contact):
@@ -42,14 +51,18 @@ def NewContact():
     print("donothing");
 
 
+#class dropDown(tk.Frame):
+    #def __init__(self, parent, var, 
 
 
 
-class SimpleTable(ttk.Frame):
+class SimpleTable(tk.Frame):
     def __init__(self, parent):
-        self.contacts=[];
+        #self.contacts=[];
         self._widgets=[];
-        ttk.Frame.__init__(self, parent, background='black');
+        # use black background so it "peeks through" to 
+        # form grid lines
+        tk.Frame.__init__(self, parent, background='black');
         self._widgets = []
 
     def set(self, row, column, value):
@@ -57,34 +70,39 @@ class SimpleTable(ttk.Frame):
         widget.configure(text=value)
 
 
-    def add_contact(self, contact_info):
-        self.contacts.append(contact_info);
+    #def add_contact(self, contact_info):
+        #self.contacts.append(contact_info);
 
 
-    def print_contacts(self):
+    def print_contacts(self, contacts):
         row=0;
-        for r in self.contacts:
+        cntr=0;
+        for r in contacts:
             column=0
             current_row = []
             for c in r:
-                print("{} {} {}".format(c, row, column))
-                label = ttk.Label(self, text=c, 
-                                 borderwidth=0, width=15)
+                #print("{} {} {}".format(c, row, column))
+                #label = tk.Label(self, text=c, borderwidth=0, width=15)
+                label=Text(self, height=1, width=15);
+                label.insert(INSERT, c);
+                if(row==0):
+                    label.config(state=DISABLED);
                 label.grid(row=row, column=column, sticky="nsew", padx=1, pady=1)
                 current_row.append(label)
                 column+=1;
             self._widgets.append(current_row)
             row+=1;
+            
 
         for column in range(7):
             self.grid_columnconfigure(column, weight=1)
 
 
 
-class StartPage(ttk.Frame):
+class StartPage(tk.Frame):
 
     def __init__(self, parent, controller):
-        ttk.Frame.__init__(self, parent)
+        tk.Frame.__init__(self, parent)
 
         fname=StringVar();
         lname=StringVar();
@@ -101,6 +119,7 @@ class StartPage(ttk.Frame):
         filemenu = Menu(menubar, tearoff=0);
         menubar.add_cascade(label="File", menu=filemenu);
         filemenu.add_command(label="New", command=donothing);
+        filemenu.add_command(label="Open", command=donothing);
         filemenu.add_command(label="Save", command=donothing);
         filemenu.add_command(label="Save as", command=donothing);
         filemenu.add_command(label="Import", command=donothing);
@@ -118,36 +137,46 @@ class StartPage(ttk.Frame):
 
 
         #Main frame
-        #mainframe=ttk.Frame(root, padding="5 20 10 10");
+        #mainframe=tk.Frame(root, padding="5 20 10 10");
         #mainframe.grid(column=0, row=0, sticky=(N, W, E, S));
 
-        #ttk.Label(self, text="First Name").grid(column=2, row=2, padx=10, pady=5);
-        #ttk.Label(self, text="Last Name").grid(column=3, row=2, padx=10, pady=5);
-        #ttk.Label(self, text="Address").grid(column=4, row=2, padx=10, pady=5);
-        #ttk.Label(self, text="State").grid(column=5, row=2, padx=10, pady=5);
-        #ttk.Label(self, text="Zip").grid(column=6, row=2, padx=10, pady=5);
-        #ttk.Label(self, text="Email").grid(column=7, row=2, padx=10, pady=5);
-        #ttk.Label(self, text="Phone Number").grid(column=8, row=2, padx=10, pady=5);
+        #tk.Label(self, text="First Name").grid(column=2, row=2, padx=10, pady=5);
+        #tk.Label(self, text="Last Name").grid(column=3, row=2, padx=10, pady=5);
+        #tk.Label(self, text="Address").grid(column=4, row=2, padx=10, pady=5);
+        #tk.Label(self, text="State").grid(column=5, row=2, padx=10, pady=5);
+        #tk.Label(self, text="Zip").grid(column=6, row=2, padx=10, pady=5);
+        #tk.Label(self, text="Email").grid(column=7, row=2, padx=10, pady=5);
+        #tk.Label(self, text="Phone Number").grid(column=8, row=2, padx=10, pady=5);
 
-        ttk.Label(self, textvariable=fname).grid(column=2, row=3, padx=10, pady=5);
-        ttk.Label(self, textvariable=lname).grid(column=3, row=3, padx=10, pady=5);
-        ttk.Label(self, textvariable=address).grid(column=4, row=3, padx=10, pady=5);
-        ttk.Label(self, textvariable=state).grid(column=5, row=3, padx=10, pady=5);
-        ttk.Label(self, textvariable=zipC).grid(column=6, row=3, padx=10, pady=5);
-        ttk.Label(self, textvariable=email).grid(column=7, row=3, padx=10, pady=5);
-        ttk.Label(self, textvariable=phone_number).grid(column=8, row=3, padx=10, pady=5);
+        tk.Label(self, textvariable=fname).grid(column=2, row=3, padx=10, pady=5);
+        tk.Label(self, textvariable=lname).grid(column=3, row=3, padx=10, pady=5);
+        tk.Label(self, textvariable=address).grid(column=4, row=3, padx=10, pady=5);
+        tk.Label(self, textvariable=state).grid(column=5, row=3, padx=10, pady=5);
+        tk.Label(self, textvariable=zipC).grid(column=6, row=3, padx=10, pady=5);
+        tk.Label(self, textvariable=email).grid(column=7, row=3, padx=10, pady=5);
+        tk.Label(self, textvariable=phone_number).grid(column=8, row=3, padx=10, pady=5);
+
+
+        #Sort by meu
+        sort_label=ttk.Label(self, text="Sort by:");
+        sort_label.grid(row=0, column=6, stick="e");
+        var = StringVar(self);
+        options = ["Name", "Zip"];
+        var.set(options[0]);
+        dropdown = ttk.OptionMenu(self, var, options[0], *options);
+        dropdown.grid(row=0, column=7, sticky="w");
 
         
         t = SimpleTable(self);
-        t.grid(row=0, column=0, columnspan=8);
-        t.add_contact(parent.contacts);
-        t.print_contacts();
+        t.grid(row=1, column=0, columnspan=8, padx=20);
+        #t.add_contact(contacts);
+        t.print_contacts(contacts);
 
 
-class PageOne(ttk.Frame):
+class PageOne(tk.Frame):
 
     def __init__(self, parent, controller):
-        ttk.Frame.__init__(self, parent)
+        tk.Frame.__init__(self, parent)
         self.parent=parent;
         self.controller = controller;
 
@@ -163,61 +192,70 @@ class PageOne(ttk.Frame):
         phone=StringVar();
 
         #Collect first name from user.
-        ttk.Label(self, text="first name").grid(column=2, row=2, sticky=(W, E))
+        tk.Label(self, text="first name").grid(column=2, row=2, sticky=(W, E))
         first_name=ttk.Entry(self, width=7, textvariable=fname);
         first_name.grid(column=2, row=3, sticky=(W, E));
-        new_contact.append(fname);
+        #new_contact.append(fname);
 
         #Collect last name from user.
-        ttk.Label(self, text="last name").grid(column=3, row=2, sticky=(W, E))
+        tk.Label(self, text="last name").grid(column=3, row=2, sticky=(W, E))
         last_name=ttk.Entry(self, width=7, textvariable=lname);
         last_name.grid(column=3, row=3, sticky=(W, E));
-        new_contact.append(lname);
+        #new_contact.append(lname);
 
         #Collect email from user.
-        ttk.Label(self, text="email").grid(column=2, row=4, sticky=(W, E))
+        tk.Label(self, text="email").grid(column=2, row=4, sticky=(W, E))
         em=ttk.Entry(self, width=7, textvariable=email);
         em.grid(column=2, row=5, sticky=(W, E));
-        new_contact.append(email);
+        #new_contact.append(email);
 
 
         #Collect address from user
-        ttk.Label(self, text="Address").grid(column=2, row=6, sticky=(W, E))
+        tk.Label(self, text="Address").grid(column=2, row=6, sticky=(W, E))
         addr=ttk.Entry(self, width=7, textvariable=address);
         addr.grid(column=2, row=7, sticky=(W, E));
-        new_contact.append(address);
+        #new_contact.append(address);
         
 
 
         #Collect state from user
-        ttk.Label(self, text="State").grid(column=3, row=6, sticky=(W, E))
+        tk.Label(self, text="State").grid(column=3, row=6, sticky=(W, E))
         st=ttk.Entry(self, width=7, textvariable=state);
         st.grid(column=3, row=7, sticky=(W, E));
-        new_contact.append(state);
+        #new_contact.append(state);
 
 
         #Collect zip from user
-        ttk.Label(self, text="Zip Code").grid(column=2, row=8, sticky=(W, E))
+        tk.Label(self, text="Zip Code").grid(column=2, row=8, sticky=(W, E))
         zip_code=ttk.Entry(self, width=25, textvariable=zipC);
         zip_code.grid(column=2, row=9, sticky=(W, E));
-        new_contact.append(zipC);
+        #new_contact.append(zipC);
         
         #Collect zip from user
-        ttk.Label(self, text="Zip Code").grid(column=2, row=9, sticky=(W, E))
+        tk.Label(self, text="Phone Number").grid(column=2, row=10, sticky=(W, E))
         phone_number=ttk.Entry(self, width=25, textvariable=phone);
-        phone_number.grid(column=2, row=9, sticky=(W, E));
-        new_contact.append(phone);
+        phone_number.grid(column=2, row=11, sticky=(W, E));
+        #new_contact.append(phone);
 
 
 
-        ttk.Button(self, text="submit", command=lambda: self.add_contact(new_contact)).grid(column=2, row=10);
-        ttk.Button(self, text="cancle", command=lambda: controller.show_frame(StartPage)).grid(column=3, row=10);
+
+        ttk.Button(self, text="submit", command=lambda: self.add_contact(first_name.get(), last_name.get(), address.get(), state.get(), zipC.get(), em.get(), phone.get())).grid(column=2, row=12);
+        ttk.Button(self, text="cancle", command=lambda: controller.show_frame(StartPage)).grid(column=3, row=12);
 
         for child in self.winfo_children(): child.grid_configure(padx=5, pady=5);
 
-    def add_contact(self, contact):
-        self.parent.contacts.append(contact);
+
+
+    def add_contact(self, fname, lname, address, state, zipC, email, phone):
+        new_contact=[fname, lname, address, state, zipC, email, phone];
+        #print(new_contact);
+        contacts.append(new_contact);
+        #self.controller.refresh(StartPage);
+        self.controller.refresh_frame(StartPage);
         self.controller.show_frame(StartPage);
+        #self.parent.contacts.append(contact);
+        #self.controller.show_frame(StartPage);
 
 
 
