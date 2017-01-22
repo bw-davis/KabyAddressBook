55b816b3cb63d096bb13f066c844fe923af8e9ba
ahill7 <ahill7@uoregon.edu>
2017-01-21 18:20:51 -0800

    added delete page

--- addressbook/gui.py
+++ addressbook/gui.py
@@ -4,9 +4,9 @@ from tkinter import ttk
 import tkinter.filedialog 
 from AddressBook import *
 
-contacts=["First name", "Last name", "Address", "State", "Zip", "Email", "Phone Number"];
+contacts=["First name", "Last name", "Address1", "Address2", "City", "State", "Zip", "Email", "Phone Number"];
 book = AddressBook()
-book.importFromFile("SavedAddressBook.txt")
+book.importFromFile("SavedAddressBook.tsv")
 
 class KabyAddrapp(tk.Tk):
 
@@ -19,11 +19,8 @@ class KabyAddrapp(tk.Tk):
             print("need to make a book");
 
         tk.Tk.__init__(self, *args, **kwargs);
-        #This command will add logo
-        #tk.Tk.conbitmap(self, default="");
         self.container = tk.Frame(self); 
 
-        #container.pack(side="top", fill="both", expand=True);
         self.container.grid(row=0, ipadx=25, ipady=10);
 
         self.container.grid_rowconfigure(0,weight=1);
@@ -31,7 +28,7 @@ class KabyAddrapp(tk.Tk):
         
         self.frames= {};
         
-        for F in (StartPage, PageOne):
+        for F in (StartPage, PageOne, DeletePage):
             frame = F(self.container, self);
             self.frames[F] = frame;
             frame.grid(row=0, column=0, sticky="nsew");
@@ -77,29 +74,17 @@ def openAddressBook():
 
 def save():
         print("dosomething");
-        #FileName=tkinter.filedialog.askopenfilename()
-        #FileName=tk.filedialog.asksaveasfilename(filetypes=[("Python源文件",".py")])
-        #print (FileName)
         print("savefile")
 
 def saveAs():
         print("dosomething");
-        #FileName=tkinter.filedialog.askopenfilename()
         FileName=tk.filedialog.asksaveasfilename(filetypes=[("text",".txt")])
         print (FileName)
 
 
-#class dropDown(tk.Frame):
-    #def __init__(self, parent, var, 
-
-
-
-class SimpleTable(tk.Frame):
+class SimpleTable2(tk.Frame):
     def __init__(self, parent):
-        #self.contacts=[];
         self._widgets=[];
-        # use black background so it "peeks through" to 
-        # form grid lines
         tk.Frame.__init__(self, parent, background='black');
         self._widgets = []
 
@@ -108,34 +93,11 @@ class SimpleTable(tk.Frame):
         widget.configure(text=value)
 
 
-    #def add_contact(self, contact_info):
-        #self.contacts.append(contact_info);
-
-    #def print_contacts(self, contacts):
-        #row=0;
-        #cntr=0;
-        #for r in contacts:
-            #column=0
-            #current_row = []
-            #for c in r:
-                #print("{} {} {}".format(c, row, column))
-                #label = tk.Label(self, text=c, borderwidth=0, width=15)
-                #label=Text(self, height=1, width=15);
-                #label.insert(INSERT, c);
-                #if(row==0):
-                    #label.config(state=DISABLED);
-                #label.grid(row=row, column=column, sticky="nsew", padx=1, pady=1)
-                #current_row.append(label)
-                #column+=1;
-            #self._widgets.append(current_row)
-            #row+=1;
-
     def print_contacts(self, contacts):
         row=0;
         column=0
         current_row = []
-        for c in contacts:
-            #print("{} {} {}".format(c, row, column));
+        for c in ["Delete","First name", "Last name", "Address1", "Address2", "City", "State", "Zip", "Email", "Phone Number"]:
             label=Text(self, height=1, width=15);
             label.insert(INSERT, c);
             label.config(state=DISABLED);
@@ -148,7 +110,61 @@ class SimpleTable(tk.Frame):
         for entry in book:
             column=0;
             current_row=[];
-            for attr in ["FirstName", "LastName", "Address", "CityState", "Zipcode", "Email"]:
+            for attr in ["button","FirstName", "LastName", "Address1", "City", "Zipcode", "Phone"]:
+                if(attr=="button"):
+                    print("button")
+                    v=StringVar()
+                    v.set("L");
+
+                    b=Radiobutton(self, text="", variable=v, value="");
+                    b.grid(row=row, column=column, sticky="nsew", padx=1, pady=1);
+
+                else:
+                    t=entry.getAttribute(attr);
+                    print("{} {} {}".format(t, row, column));
+                    label=Text(self, height=1, width=15);
+                    label.insert(INSERT, t);
+                    label.grid(row=row, column=column, sticky="nsew", padx=1, pady=1)
+                    current_row.append(label)
+                column+=1;
+            self._widgets.append(current_row)
+            row+=1;
+
+            
+
+        for column in range(7):
+            self.grid_columnconfigure(column, weight=1)
+
+
+class SimpleTable(tk.Frame):
+    def __init__(self, parent):
+        self._widgets=[];
+        tk.Frame.__init__(self, parent, background='black');
+        self._widgets = []
+
+    def set(self, row, column, value):
+        widget = self._widgets[row][column]
+        widget.configure(text=value)
+
+
+    def print_contacts(self, contacts):
+        row=0;
+        column=0
+        current_row = []
+        for c in contacts:
+            label=Text(self, height=1, width=15);
+            label.insert(INSERT, c);
+            label.config(state=DISABLED);
+            label.grid(row=row, column=column, sticky="nsew", padx=1, pady=1)
+            current_row.append(label)
+            self._widgets.append(current_row)
+            column+=1;
+        row+=1;
+
+        for entry in book:
+            column=0;
+            current_row=[];
+            for attr in ["FirstName", "LastName", "Address1", "City", "Zipcode", "Phone"]:
                 t=entry.getAttribute(attr);
                 print("{} {} {}".format(t, row, column));
                 label=Text(self, height=1, width=15);
@@ -165,20 +181,11 @@ class SimpleTable(tk.Frame):
             self.grid_columnconfigure(column, weight=1)
 
 
-
-class StartPage(tk.Frame):
+class DeletePage(tk.Frame):
 
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
 
-        fname=StringVar();
-        lname=StringVar();
-        email=StringVar();
-        address=StringVar();
-        state=StringVar();
-        zipC=StringVar();
-        phone_number=StringVar();
-        email=StringVar();
 
         #File tab on menu bar
         menubar = Menu(parent.master);
@@ -199,18 +206,60 @@ class StartPage(tk.Frame):
         editmenu=Menu(menubar, tearoff=0);
         menubar.add_cascade(label="Edit", menu=editmenu);
         editmenu.add_command(label="Undo", command=donothing);
-        #addmenu=Menu(menubar, tearoff=0);
+        addmenu=Menu(menubar, tearoff=0);
         #menubar.add_cascade(label="Add", menu=addmenu);
-        menubar.add_command(label="Add", command=lambda: controller.show_frame(PageOne));
+        editmenu.add_command(label="Add", command=lambda: controller.show_frame(PageOne));
+        editmenu.add_command(label="Delete", command=lambda: controller.show_frame(DeletePage));
 
 
-        tk.Label(self, textvariable=fname).grid(column=2, row=3, padx=10, pady=5);
-        tk.Label(self, textvariable=lname).grid(column=3, row=3, padx=10, pady=5);
-        tk.Label(self, textvariable=address).grid(column=4, row=3, padx=10, pady=5);
-        tk.Label(self, textvariable=state).grid(column=5, row=3, padx=10, pady=5);
-        tk.Label(self, textvariable=zipC).grid(column=6, row=3, padx=10, pady=5);
-        tk.Label(self, textvariable=email).grid(column=7, row=3, padx=10, pady=5);
-        tk.Label(self, textvariable=phone_number).grid(column=8, row=3, padx=10, pady=5);
+
+
+        #Sort by meu
+        sort_label=ttk.Label(self, text="Sort by:");
+        sort_label.grid(row=0, column=6, stick="e");
+        var = StringVar(self);
+        options = ["Name", "Zip"];
+        var.set(options[0]);
+        dropdown = ttk.OptionMenu(self, var, options[0], *options);
+        dropdown.grid(row=0, column=7, sticky="w");
+
+        
+        t = SimpleTable2(self);
+        t.grid(row=1, column=0, columnspan=8, padx=20);
+        t.print_contacts(contacts);
+
+
+class StartPage(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+
+
+        #File tab on menu bar
+        menubar = Menu(parent.master);
+        parent.master.config(menu=menubar);
+        filemenu = Menu(menubar, tearoff=0);
+        menubar.add_cascade(label="File", menu=filemenu);
+        filemenu.add_command(label="New", command=donothing);
+        filemenu.add_command(label="Open", command=openAddressBook);
+        filemenu.add_command(label="Save", command=save);
+        filemenu.add_command(label="Save as", command=saveAs);
+        filemenu.add_command(label="Import", command=importFile);
+        filemenu.add_command(label="Export", command=exportFile);
+        filemenu.add_separator();
+        filemenu.add_command(label="Exit", command=quit);
+
+
+        #Edit tab on menu bar
+        editmenu=Menu(menubar, tearoff=0);
+        menubar.add_cascade(label="Edit", menu=editmenu);
+        editmenu.add_command(label="Undo", command=donothing);
+        addmenu=Menu(menubar, tearoff=0);
+        #menubar.add_cascade(label="Add", menu=addmenu);
+        editmenu.add_command(label="Add", command=lambda: controller.show_frame(PageOne));
+        editmenu.add_command(label="Delete", command=donothing);
+
+
 
 
         #Sort by meu
@@ -310,18 +359,11 @@ class PageOne(tk.Frame):
 
 
     def add_contact(self, fname, lname, address, state, zipC, email, phone):
-        #
-        #entry = AddressBookEntry("John", "Doe", "123 Daywood Drive", "Portland, MN", "04101", "john@gmail.com")
-        #new_contact=[fname, lname, address, state, zipC, email, phone];
-        #print(new_contact);
-        #
         new_contact=AddressBookEntry(fname, lname, address, state, zipC, email); 
         book.addEntry(new_contact);
         self.controller.refresh_frame(StartPage);
         self.controller.show_frame(StartPage);
-        book.exportToFile("SavedAddressBook.txt")
-        #self.parent.contacts.append(contact);
-        #self.controller.show_frame(StartPage);
+        book.exportToFile("SavedAddressBook.tsv");
 
 
 
