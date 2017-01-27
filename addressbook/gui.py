675f87b62c905ae4748ac412b329b0fdde3cc8a6
ahill7 <ahill7@uoregon.edu>
2017-01-27 10:51:15 -0800

    Merged Andrews and Yubos

--- addressbook/gui.py
+++ addressbook/gui.py
@@ -2,26 +2,31 @@ from tkinter import *
 import tkinter as tk 
 from tkinter import ttk
 import tkinter.filedialog 
+from tkinter.messagebox import *
 from AddressBook import *
+from tkinter import messagebox
 
-#
-#Below is a the template for how a address book entery is stored.
-#AddressBookEnetry(FirstName, LastName, Address1, Address2, City, State, Zipcode, Phone)
-#
-contacts=["First name", "Last name", "Address1", "Address2", "City", "State", "Zip", "Phone Number"];
+#contacts=["First name", "Last name", "Address1", "Address2", "City", "State", "Zip", "Email", "Phone Number"];
+#col_name = ["FirstName", "LastName", "Address1", "City", "Zipcode", "Phone"];
+contacts=["First name", "Last name", "Address1", "Address2", "City", "State", "Zip","Phone Number"];
 col_name = ["FirstName", "LastName", "Address1", "Address2", "City", "State", "Zipcode", "Phone"];
 book = AddressBook()
 book.importFromFile("SavedAddressBook.tsv")
+
 dirty=[]; #And array of arrays of[label, row, col] of all "dirty" or modified text widgets
+states=[] #record index of the conetancts that you want to delete
+print("I cleaned states in");
+edited=False;
 skip = "#skip"
 
 class KabyAddrapp(tk.Tk):
 
     def __init__(self, *args, **kwargs):
 
+        #states=[] #record delete indexs
+        #print("I cleaned states ");
         if(len(args)==1):
             print("book");
-            book.importFromFile(args[0]); 
         else:
             print("need to make a book");
 
@@ -36,11 +41,25 @@ class KabyAddrapp(tk.Tk):
         self.frames= {};
         
         for F in (StartPage, PageOne, DeletePage):
+            if F==DeletePage:
+                print("initialize StartPage")
+            if F==DeletePage:
+                print("initialize PageOne")
+            if F==DeletePage:
+                print("initialize DeletePage")
             frame = F(self.container, self);
             self.frames[F] = frame;
             frame.grid(row=0, column=0, sticky="nsew");
-
+        #states=[] #record delete index
+        #print("I cleaned states 2");
         self.show_frame(StartPage);
+
+    def __enter__(self, *args, **kwargs):
+        return self;
+
+
+    def __exit__(self, *args, **kwargs):
+        print("exiting")
 
     def show_frame(self, cont):
         frame = self.frames[cont];
@@ -62,56 +81,69 @@ class KabyAddrapp(tk.Tk):
 def donothing():
     print("donothing");
 
+
 def NewContact():
     print("donothing");
 
 def importFile():
-        print("dosomething");
-        importFileName=tkinter.filedialog.askopenfilename()
-        print (importFileName)
+    print("dosomething");
+    importFileName=tkinter.filedialog.askopenfilename()
+    print (importFileName)
 
 def exportFile():
-        print("dosomething");
-        importFileName=tkinter.filedialog.askopenfilename()
-        print (exportFileName)
+    print("dosomething");
+    importFileName=tkinter.filedialog.askopenfilename()
+    print (exportFileName)
 
 def openAddressBook():
-        print("dosomething");
-        AddressbookName=tkinter.filedialog.askopenfilename()
-        book2 = AddressBook()
-        book2.importFromFile("SavedAddressBook.tsv")
-        app2=KabyAddrapp(AddressbookName);
-        app2.mainloop();
-        print (AddressbookName)
+    #print("dosomething");
+    AddressbookName=tkinter.filedialog.askopenfilename()
+    book2 = AddressBook()
+    book2.importFromFile("SavedAddressBook.tsv")
+    app2=KabyAddrapp(AddressbookName);
+    app2.mainloop();
+    print (AddressbookName)
 
 def save():
-        print("dosomething");
-        print("savefile")
+    #print("dosomething");
+    print("savefile")
+    book.exportToFile("SavedAddressBook.tsv");
 
 def saveAs():
-        print("dosomething");
-        FileName=tk.filedialog.asksaveasfilename(filetypes=[("text",".txt")])
-        print (FileName)
+    #print("dosomething");
+    FileName=tk.filedialog.asksaveasfilename(filetypes=[("text",".tsv")])
+    print (FileName)
+    book.exportToFile(FileName);
+
+
+
 
 
 class SimpleTable2(tk.Frame):
-
     def __init__(self, parent):
+        print("im in SimpleTable2");
+        states=[];
         self._widgets=[];
         tk.Frame.__init__(self, parent, background='black');
         self._widgets = []
 
-
     def set(self, row, column, value):
         widget = self._widgets[row][column]
         widget.configure(text=value)
+    def onPress(self,i):
+        states[i]= 1;
 
 
     def print_contacts(self, contacts):
-        col_name = ["Delete", "FirstName", "LastName", "Address1", "Address2", "City", "State", "Zipcode", "Phone"];
+        #states=[];
+        
+        #print("I cleaned states 3");
+        col_name = ["Delete","FirstName", "LastName", "Address1", "Address2", "City", "State", "Zipcode", "Phone"];
         row=0;
         column=0
         current_row = []
+        index=0;
+        #for c in ["Delete","First name", "Last name", "Address1", "Address2", "City", "State", "Zip", "Email", "Phone Number"]:
         for c in col_name:
             label=Text(self, height=1, width=15);
             label.insert(INSERT, c);
@@ -122,41 +154,59 @@ class SimpleTable2(tk.Frame):
             column+=1;
         row+=1;
 
+
         for entry in book:
             column=0;
             current_row=[];
+            #for attr in ["button","FirstName", "LastName", "Address1", "City", "Zipcode", "Phone"]:
+                #if(attr=="button"):
             for attr in col_name:
                 if(attr=="Delete"):
                     print("button")
                     v=StringVar()
+                    z=IntVar()
                     v.set("L");
-
-                    b=Radiobutton(self, text="", variable=v, value="");
+                    #b=Radiobutton(self, text="", variable=v);
+                    b=Checkbutton(self, text=row,command=(lambda i=index:self.onPress(i)));#creat check buttons in when we create the table, bind to onPress funtion
                     b.grid(row=row, column=column, sticky="nsew", padx=1, pady=1);
+                    states.append(0);
+                    print("now i am appending")
+
 
                 else:
                     t=entry.getAttribute(attr);
                     print("{} {} {}".format(t, row, column));
                     label=Text(self, height=1, width=15);
-                    label.insert(INSERT, t);
+                    if t == skip:
+                        label.insert(INSERT, "");
+                    else:
+                        label.insert(INSERT, t);
                     label.grid(row=row, column=column, sticky="nsew", padx=1, pady=1)
                     current_row.append(label)
+
                 column+=1;
             self._widgets.append(current_row)
+            index+=1;
             row+=1;
+        print(states)
+
 
             
 
         for column in range(7):
             self.grid_columnconfigure(column, weight=1)
-
+        index=0;
 
 
 class SimpleTable(tk.Frame):
     def __init__(self, parent):
+        print("im in SimpleTable1");
         self._widgets=[];
         tk.Frame.__init__(self, parent, background='black');
         self._widgets = []
+        #states=[] #record delete index
+        #print("I cleaned states 4");
+
 
     def set(self, row, column, value):
         widget = self._widgets[row][column]
@@ -172,10 +222,10 @@ class SimpleTable(tk.Frame):
         #print(contact_attr);
         new_contact_data = entry.get("1.0", END).replace('\n', ''); #The text that has been entered in the Text widget. The end-1c ignores newline charater
         #print(new_contact_data);
+        entry.delete("2.0", END);
         entry.tag_add("a", "1.0", END);
         entry.tag_configure("a", background="skyblue");
         #entry.insert(INSERT, new_contact_data);
-        entry.delete("2.0", END);
         dirty.append([entry, row, col]); #Keeping track of all unsaved enteiries.
         contact.setAttribute(attr, new_contact_data);
         book.exportToFile("SavedAddressBook.tsv");
@@ -218,41 +268,23 @@ class SimpleTable(tk.Frame):
         for column in range(7):
             self.grid_columnconfigure(column, weight=1)
 
+    def toDelete():
+        #tast case for delete
+        pass
+
 
 class DeletePage(tk.Frame):
 
+
     def __init__(self, parent, controller):
+        print("im in DeletePage");
         tk.Frame.__init__(self, parent)
+        self.parent=parent;
+        self.controller = controller;
+        states=[] #record delete index
+        #print("I cleaned states 5");
 
 
-        #File tab on menu bar
-        menubar = Menu(parent.master);
-        parent.master.config(menu=menubar);
-        filemenu = Menu(menubar, tearoff=0);
-        menubar.add_cascade(label="File", menu=filemenu);
-        filemenu.add_command(label="New", command=donothing);
-        filemenu.add_command(label="Open", command=openAddressBook);
-        filemenu.add_command(label="Save", command=save);
-        filemenu.add_command(label="Save as", command=saveAs);
-        filemenu.add_command(label="Import", command=importFile);
-        filemenu.add_command(label="Export", command=exportFile);
-        filemenu.add_separator();
-        filemenu.add_command(label="Exit", command=quit);
-
-
-        #Edit tab on menu bar
-        editmenu=Menu(menubar, tearoff=0);
-        menubar.add_cascade(label="Edit", menu=editmenu);
-        editmenu.add_command(label="Undo", command=donothing);
-        addmenu=Menu(menubar, tearoff=0);
-        #menubar.add_cascade(label="Add", menu=addmenu);
-        editmenu.add_command(label="Add", command=lambda: controller.show_frame(PageOne));
-        editmenu.add_command(label="Delete", command=lambda: controller.show_frame(DeletePage));
-
-
-
-
-        #Sort by meu
         sort_label=ttk.Label(self, text="Sort by:");
         sort_label.grid(row=0, column=6, stick="e");
         var = StringVar(self);
@@ -266,19 +298,51 @@ class DeletePage(tk.Frame):
         t.grid(row=1, column=0, columnspan=8, padx=20);
         t.print_contacts(contacts);
 
-        ttk.Button(self, text="Delete", command=donothing).grid(row=2, column=2, stick='e');
+        ttk.Button(self, text="Delete", command=lambda: self.delete_confirm()).grid(row=2, column=2, stick='e');
         #ttk.Button(self, text="Cancle", command=).grid(row=3, column=2);
         ttk.Button(self, text="Cancle", command=lambda: controller.show_frame(StartPage)).grid(column=3, row=2, stick='w');
+
+    def delete_confirm(self):
+        #the function that will be involk when user click Delete button in delete page
+        index=0
+        count =0
+        print("donsomething2");
+        print(states);
+        if askokcancel("Delete","Are you sure to Delete selected Data?"):
+            # pop a diolog let user to confirm
+            print("yes")
+            #if yes, delete contacts
+            for i in states:
+                if i !=0:
+                    print("should delete No.")
+                    print(index+1)
+                    book.removeEntry(index-count);
+                    count+=1
+                index+=1;
+
+            book.exportToFile("SavedAddressBook.tsv");
+            self.controller.refresh_frame(StartPage);
+            self.controller.show_frame(StartPage);
+        else:
+            #if user cancles
+            print("No")
 
 
 class StartPage(tk.Frame):
 
     def __init__(self, parent, controller):
+        print("im in StartPage");
         tk.Frame.__init__(self, parent)
-        self.controller=controller;
-
+        self.parent=parent;
+        self.controller = controller;
+        #reset array states for next delete operation
+        #states =[]
+        #print("I cleaned states 6");
+        #resetDeletePage
+        #self.controller.refresh_frame(DeletePage);
 
         #File tab on menu bar
+        print(states)
         menubar = Menu(parent.master);
         parent.master.config(menu=menubar);
         filemenu = Menu(menubar, tearoff=0);
@@ -296,16 +360,16 @@ class StartPage(tk.Frame):
         #Edit tab on menu bar
         editmenu=Menu(menubar, tearoff=0);
         menubar.add_cascade(label="Edit", menu=editmenu);
-        editmenu.add_command(label="Undo", command=donothing);
+        #editmenu.add_command(label="Undo", command=donothing);
         addmenu=Menu(menubar, tearoff=0);
         #menubar.add_cascade(label="Add", menu=addmenu);
         editmenu.add_command(label="Add", command=lambda: controller.show_frame(PageOne));
-        editmenu.add_command(label="Delete", command=donothing);
+        #editmenu.add_command(label="Delete", command=lambda: controller.show_frame(DeletePage));
+        editmenu.add_command(label="Delete", command=self.to_delete_page);
 
 
 
-
-        #Sort by meu
+         #Sort by meu
         sort_label=ttk.Label(self, text="Sort by:");
         sort_label.grid(row=0, column=6, stick="e");
         var = StringVar(self);
@@ -332,12 +396,25 @@ class StartPage(tk.Frame):
         self.controller.show_frame(StartPage);
 
 
+    def to_delete_page(self): 
+        # click delete in menu bar
+        global states
+        states=[] #record delete index
+        print("I cleaned states 7");
+        self.controller.refresh_frame(DeletePage);
+        print("Im going to the delete page")
+        self.controller.show_frame(DeletePage)
+
+
+
+
         
 
 
 class PageOne(tk.Frame):
 
     def __init__(self, parent, controller):
+        print("im in PageOne");
         tk.Frame.__init__(self, parent)
         self.parent=parent;
         self.controller = controller;
@@ -402,18 +479,18 @@ class PageOne(tk.Frame):
         phone_number=ttk.Entry(self, width=25, textvariable=phone);
         phone_number.grid(column=2, row=11, sticky=(W, E));
         #new_contact.append(phone);
-
-        tk.Label(self, text="Address 2").grid(column=3, row=6, sticky=(W, E));
-        address2=ttk.Entry(self, width=25);
-        address2.grid(column=3, row=7, sticky=(W, E));
-
+        tk.Label(self, text="Address2").grid(column=3, row=6, sticky=(W, E))
+        phone_number=ttk.Entry(self, width=25);
+        phone_number.grid(column=3, row=7, sticky=(W, E));
 
         #Bind Enter to create customer as well.
 
-        ttk.Button(self, text="submit", command=lambda: self.add_contact(first_name.get(), last_name.get(), address.get(), "", city.get(),  state.get(), zipC.get(), em.get(), phone.get())).grid(column=2, row=12);
+        #ttk.Button(self, text="submit", command=lambda: self.add_contact(first_name.get(), last_name.get(), address.get(), state.get(), zipC.get(), em.get(), phone.get())).grid(column=2, row=12);
+        ttk.Button(self, text="submit", command=lambda: self.add_contact(first_name.get(), last_name.get(), address.get(),"",city.get(), state.get(), zipC.get(), em.get(), phone.get())).grid(column=2, row=12);
         ttk.Button(self, text="cancle", command=lambda: controller.show_frame(StartPage)).grid(column=3, row=12);
 
         for child in self.winfo_children(): child.grid_configure(padx=5, pady=5);
+
 
 
     #AddressBookEnetry(FirstName, LastName, Address1, Address2, City, State, Zipcode, Phone)
@@ -431,14 +508,19 @@ class PageOne(tk.Frame):
         self.controller.show_frame(StartPage);
         book.exportToFile("SavedAddressBook.tsv");
 
-
-
+def on_closing(root):
+    if messagebox.askokcancel("Quit", "Do you want to quit?"):
+        root.destroy()
 
 
 def main():
     app=KabyAddrapp();
+    app.protocol("WM_DELETE_WINDOW", lambda: on_closing(app));
     app.mainloop();
 
 
 if __name__ == "__main__":
     main()
+
+
+
